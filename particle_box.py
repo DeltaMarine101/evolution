46300b940af9a266b61145431b304196941311bd
Jeremie Kull <jeremie@spotto.biz>
2019-05-18 19:30:21 +1000

    Added triangles and normal visualisation

--- particle_box.py
+++ particle_box.py
@@ -4,7 +4,7 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.graphics import Color, Ellipse, Rectangle, Triangle, Bezier
 from kivy.core.window import Window
 
 from functools import partial
@@ -36,8 +36,8 @@ class Particle:
 class Block:
     def __init__(self, x, y, w, h, id):
         self.color = [rnd(), rnd(), 1]
-        self.faces = [(x, y + h/2), (x + w, y + h/2), (x + w/2, y), (x + w/2, y + h)]
-        self.normals = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        self.faces = [[x, y + h/2], [x + w, y + h/2], [x + w/2, y], [x + w/2, y + h]]
+        self.normal = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         self.pos = [x, y]
         self.size = (w, h)
         self.id = id
@@ -59,10 +59,101 @@ class Block:
         return True
 
 
-class EvoCanvasApp(App):
+class Polygon3:
+    def __init__(self, x0, y0, x1, y1, x2, y2, id):
+        self.color = [rnd(), rnd(), 1]
+        self.vertex = [[x0, y0], [x1, y1], [x2, y2]]
+        norm = [m.sqrt((x0 - x1)**2 + (y0 - y1)**2), m.sqrt((x1 - x2)**2 + (y1 - y2)**2), m.sqrt((x2 - x0)**2 + (y2 - y0)**2)]
+        self.normal = [((y1 - y0) / norm[0], (x0 - x1) / norm[0]),
+                        ((y2 - y1) / norm[1], (x1 - x2) / norm[1]),
+                        ((y0 - y2) / norm[2], (x2 - x0) / norm[2])]
+        self.id = id
+
+    def check_collision(self, p):
+        pw, ph = p.size
+        px, py = p.pos[0] + pw / 2, p.pos[1] + ph / 2
+
+        x0, y0 = (self.vertex[0][0] + self.vertex[1][0]) / 2, (self.vertex[0][1] + self.vertex[1][1]) / 2
+        if self.normal[0][1] != 0:
+            m_1 = - self.normal[0][0] / self.normal[0][1]
+            b = self.vertex[0][1] - m_1 * self.vertex[0][0]
+            d0 = abs(-m_1 * px + py - b) / m.sqrt(1 + m_1**2)
+
+            if m_1 != 0:
+                m_2 = -1 / m_1
+                b_2 = y0 - m_2 * x0
+                l0 = abs(-m_2 * px + py - b_2) / m.sqrt(1 + m_2**2)
+            else:
+                l0 = abs(x0 - px)
+        else:
+            d0 = abs(x0 - px)
+            l0 = abs(y0 - py)
+
+        x1, y1 = (self.vertex[1][0] + self.vertex[2][0]) / 2, (self.vertex[1][1] + self.vertex[2][1]) / 2
+        if self.normal[1][1] != 0:
+            m_1 = - self.normal[1][0] / self.normal[1][1]
+            b = self.vertex[1][1] - m_1 * self.vertex[1][0]
+            d1 = abs(-m_1 * px + py - b) / m.sqrt(1 + m_1**2)
+
+            if m_1 != 0:
+                m_2 = -1 / m_1
+                b_2 = y1 - m_2 * x1
+                l1 = abs(-m_2 * px + py - b_2) / m.sqrt(1 + m_2**2)
+            else:
+                l1 = abs(x1 - px)
+        else:
+            d1 = abs(x1 - px)
+            l1 = abs(y1 - py)
+
+        x2, y2 = (self.vertex[2][0] + self.vertex[0][0]) / 2, (self.vertex[2][1] + self.vertex[0][1]) / 2
+        if self.normal[2][1] != 0:
+            m_1 = - self.normal[2][0] / self.normal[2][1]
+            b = self.vertex[2][1] - m_1 * self.vertex[2][0]
+            d2 = abs(-m_1 * px + py - b) / m.sqrt(1 + m_1**2)
+
+            if m_1 != 0:
+                m_2 = -1 / m_1
+                b_2 = y2 - m_2 * x2
+                l2 = abs(-m_2 * px + py - b_2) / m.sqrt(1 + m_2**2)
+            else:
+                l2 = abs(x2 - px)
+        else:
+            d2 = abs(x2 - px)
+            l2 = abs(y2 - py)
+
+        w0 = m.sqrt((self.vertex[0][0] - self.vertex[1][0])**2 + (self.vertex[0][1] - self.vertex[1][1])**2)
+        if d0 <= pw / 2 and l0 <= w0 / 2 + pw / 2:
+            return (0, pw / 2 - d0 + 1)
+        w1 = m.sqrt((self.vertex[1][0] - self.vertex[2][0])**2 + (self.vertex[1][1] - self.vertex[2][1])**2)
+        if d1 <= pw / 2 and l1 <= w1 / 2 + pw / 2:
+            return (1, pw / 2 - d1 + 1)
+        w2 = m.sqrt((self.vertex[0][0] - self.vertex[2][0])**2 + (self.vertex[0][1] - self.vertex[2][1])**2)
+        if d2 <= pw / 2 and l2 <= w2 / 2 + pw / 2:
+            return (2, pw / 2 - d2 + 1)
+
+class ParticleBox(App):
     def draw(self):
         self.wid.canvas.clear()
         with self.wid.canvas:
+            for i in self.triangles:
+                Color(*i.color)
+                Triangle(points=([i.vertex[0][0] + self.wid.x, i.vertex[0][1] + self.wid.y,
+                                 i.vertex[1][0] + self.wid.x, i.vertex[1][1] + self.wid.y,
+                                 i.vertex[2][0] + self.wid.x, i.vertex[2][1] + self.wid.y]))
+                Color((1, 1, 1))
+                if self.show_norms:
+                    Bezier(points=[(i.vertex[0][0] + i.vertex[1][0]) / 2 + self.wid.x,
+                                   (i.vertex[0][1] + i.vertex[1][1]) / 2 + self.wid.y,
+                                   (i.vertex[0][0] + i.vertex[1][0]) / 2 + i.normal[0][0] * 20 + self.wid.x,
+                                   (i.vertex[0][1] + i.vertex[1][1]) / 2 + i.normal[0][1] * 20 + self.wid.y])
+                    Bezier(points=[(i.vertex[1][0] + i.vertex[2][0]) / 2 + self.wid.x,
+                                   (i.vertex[1][1] + i.vertex[2][1]) / 2 + self.wid.y,
+                                   (i.vertex[1][0] + i.vertex[2][0]) / 2 + i.normal[1][0] * 20 + self.wid.x,
+                                   (i.vertex[1][1] + i.vertex[2][1]) / 2 + i.normal[1][1] * 20 + self.wid.y])
+                    Bezier(points=[(i.vertex[2][0] + i.vertex[0][0]) / 2 + self.wid.x,
+                                   (i.vertex[2][1] + i.vertex[0][1]) / 2 + self.wid.y,
+                                   (i.vertex[2][0] + i.vertex[0][0]) / 2 + i.normal[2][0] * 20 + self.wid.x,
+                                   (i.vertex[2][1] + i.vertex[0][1]) / 2 + i.normal[2][1] * 20 + self.wid.y])
             for i in self.blocks:
                 Color(*i.color)
                 Rectangle(pos=(i.pos[0] + self.wid.x, i.pos[1] + self.wid.y), size=i.size)
@@ -83,8 +174,9 @@ class EvoCanvasApp(App):
             m1 = p2.mass / p1.mass
             m2 = p1.mass / p2.mass
 
-            print("pew", p1.pos[0])
-            p1.vel, p2.vel = [m1 * p2.vel[0] * self.e_loss, m1 * p2.vel[1] * self.e_loss], [m2 * p1.vel[0] * self.e_loss, m2 * p1.vel[1] * self.e_loss]
+            p1.vel, p2.vel = [m1 * p2.vel[0] * self.e_loss,
+                              m1 * p2.vel[1] * self.e_loss], [m2 * p1.vel[0] * self.e_loss,
+                              m2 * p1.vel[1] * self.e_loss]
 
             if x1 != p2.pos[0]:
                 g = m.atan(abs(y1 - y2) / abs(x1 - x2))
@@ -100,7 +192,7 @@ class EvoCanvasApp(App):
     def b_collision(self, p, b):
         if b.check_collision(p):
             t = [(p.pos[0] + p.size[0] / 2 - i[0])**2 + (p.pos[1] + p.size[1] / 2 - i[1])**2 for i in b.faces]
-            n = b.normals[t.index(min(t))]
+            n = b.normal[t.index(min(t))]
             f = b.faces[t.index(min(t))]
 
             n_dot_v = n[0] * p.vel[0] + n[1] * p.vel[1]
@@ -109,6 +201,18 @@ class EvoCanvasApp(App):
             p.pos[0] += n[0] * (min([abs(f[0] - p.pos[0]), abs(f[0] - p.pos[0] - p.size[0])]) + 1)
             p.vel[1] = (p.vel[1] - 2 * n[1] * n_dot_v) * self.e_loss
             p.pos[1] += n[1] * (min([abs(f[1] - p.pos[1]), abs(f[1] - p.pos[1] - p.size[1])]) + 1)
+
+    def t_collision(self, p, t):
+        v = t.check_collision(p)
+        if v:
+            n = t.normal[v[0]]
+
+            n_dot_v = n[0] * p.vel[0] + n[1] * p.vel[1]
+
+            p.vel[0] = (p.vel[0] - 2 * n[0] * n_dot_v) * self.e_loss
+            p.pos[0] += n[0] * v[1]
+            p.vel[1] = (p.vel[1] - 2 * n[1] * n_dot_v) * self.e_loss
+            p.pos[1] += n[1] * v[1]
 
     def wall_collision(self, p1):
         if p1.pos[0] + p1.size[0] > self.w:
@@ -133,6 +237,9 @@ class EvoCanvasApp(App):
 
             for b in self.blocks:
                 self.b_collision(p1, b)
+
+            for t in self.triangles:
+                self.t_collision(p1, t)
 
             mx = self.mouse[0] - self.wid.x
             my = self.mouse[1] - self.wid.y
@@ -164,7 +271,8 @@ class EvoCanvasApp(App):
         Clock.schedule_once(self.tick)
 
     def add(self, n, *largs):
-        self.particles += [Particle(rnd() * self.w, rnd() * self.h, self.speed, self.size, len(self.particles) + i, self.w, self.h) for i in range(n)]
+        self.particles += [Particle(rnd() * self.w, rnd() * self.h, self.speed,
+                           self.size, len(self.particles) + i, self.w, self.h) for i in range(n)]
         self.label1.text = str(len(self.particles))
 
     def sub(self, n, *largs):
@@ -207,6 +315,8 @@ class EvoCanvasApp(App):
             self.grav = True
         if args[1] == 308:
             self.e_loss = self.inelasticity
+        if args[1] == 110:
+            self.show_norms = True
 
     def on_key_up(self, *args):
         if args[1] == 305:
@@ -215,6 +325,8 @@ class EvoCanvasApp(App):
             self.grav = False
         if args[1] == 308:
             self.e_loss = 1
+        if args[1] == 110:
+            self.show_norms = False
 
     def build(self):
         self.clicked = False
@@ -226,6 +338,7 @@ class EvoCanvasApp(App):
         self.mouse = (0, 0)
         self.ctrl = False
         self.e_loss = 1
+        self.show_norms = False
         Window.bind(mouse_pos=lambda w, p: self.set_mouse_pos(p))
         Window.bind(on_touch_down=self.on_mouse_down)
         Window.bind(on_touch_up=self.on_mouse_up)
@@ -234,7 +347,10 @@ class EvoCanvasApp(App):
         self.pos_schedule = Clock.schedule_once(self.update_mouse, 0.1)
         self.update_window_size()
 
-        self.blocks = [Block(200, 200, 200, 200, 0)]
+        self.triangles = [Polygon3(200, 200, 250, 200, 200, 250, 0),
+                          Polygon3(400, 400, 480, 420, 380, 450, 1),
+                          Polygon3(560, 50, 680, 120, 550, 250, 1)]
+        self.blocks = []#[Block(200, 200, 200, 200, 0)]
 
         self.particles = []
 
@@ -263,4 +379,4 @@ class EvoCanvasApp(App):
 
 
 if __name__ == '__main__':
-    EvoCanvasApp().run()
+    ParticleBox().run()
